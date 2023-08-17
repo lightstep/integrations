7f3a4ffc0d3ca20b641fb6ef42b5c24e7322227e
Dmitry Avdeev <dmitry.avdeev@lightstep.com>
2023-08-17 10:37:56 -0700

    azure api management name fix

--- azure/apimanagement_service/dashboards/overview/main.tf
+++ azure/apimanagement_service/dashboards/overview/main.tf
@@ -15,13 +15,13 @@ variable "lightstep_project" {
 
 output "dashboard_url" {
   value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.otel_collector_dashboard.id}"
-  description = "OpenTelemetry Collector Api Management Service Dashboard URL"
+  description = "OpenTelemetry Collector API Management Service Dashboard URL"
 }
 
 resource "lightstep_dashboard" "otel_collector_dashboard" {
   project_name   = var.lightstep_project
-  dashboard_name = "Api Management Service Metrics"
-  dashboard_description = "Monitor Api Management Service with this metrics overview dashboard."
+  dashboard_name = "API Management Service Metrics"
+  dashboard_description = "Monitor API Management Service with this metrics overview dashboard."
 
   chart {
     name = "Requests"
