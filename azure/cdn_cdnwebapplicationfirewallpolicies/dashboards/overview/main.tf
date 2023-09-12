ec71b2bbee9b5f2ac95b9a325da854e29bc1f4de
Dmitry Av <altuner@users.noreply.github.com>
2023-09-12 15:29:34 -0700

    Azure dashboards group5 1 (#27)
    Co-authored-by: Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>

---
+++ azure/cdn_cdnwebapplicationfirewallpolicies/dashboards/overview/main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = "~> 1.76.0"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
+variable "cloud_observability_project" {
+  description = "Cloud Observability Project Name"
+  type        = string
+}
+
+output "dashboard_url" {
+  value       = "https://app.lightstep.com/${var.cloud_observability_project}/dashboard/${lightstep_dashboard.otel_collector_dashboard.id}"
+  description = "OpenTelemetry Collector CDN Web Application Firewall Policies Dashboard URL"
+}
+
+resource "lightstep_dashboard" "otel_collector_dashboard" {
+  project_name          = var.cloud_observability_project
+  dashboard_name        = "CDN Web Application Firewall Policies Metrics"
+  dashboard_description = "Monitor CDN Web Application Firewall Policies with this metrics overview dashboard."
+
+  chart {
+    name = "Web Application Firewall Requests"
+    rank = "0"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_webapplicationfirewallrequestcount_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_policyname == $metadata_policyname) && (metadata_rulename == $metadata_rulename) && (metadata_action == $metadata_action)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  template_variable {
+    name                     = "azuremonitor_resource_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.resource_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_subscription_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.subscription_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_tenant_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.tenant_id"
+  }
+
+  template_variable {
+    name                     = "location"
+    default_values           = []
+    suggestion_attribute_key = "location"
+  }
+
+  template_variable {
+    name                     = "metadata_action"
+    default_values           = []
+    suggestion_attribute_key = "metadata_action"
+  }
+
+  template_variable {
+    name                     = "metadata_policyname"
+    default_values           = []
+    suggestion_attribute_key = "metadata_policyname"
+  }
+
+  template_variable {
+    name                     = "metadata_rulename"
+    default_values           = []
+    suggestion_attribute_key = "metadata_rulename"
+  }
+}
