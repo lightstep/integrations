e7ae42673d5c0eece99aa866bbee58123ed3ea1f
Heber Silva <heber.silva@lightstep.com>
2023-09-13 18:06:46 -0400

    addressed pr comments

--- collector/hikaricp/dashboards/overview/main.tf
+++ collector/hikaricp/dashboards/overview/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 resource "lightstep_dashboard" "otel_collector_hikaricp_dashboard" {
-  project_name          = var.lightstep_project
+  project_name          = var.cloud_observability_project
   dashboard_name        = "OpenTelemetry HikariCP Dashboard"
   dashboard_description = "Monitor HikariCP with this connection pool metrics summary."
 
--- collector/hikaricp/dashboards/overview/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.otel_collector_hikaricp_dashboard.id}"
-  description = "OpenTelemetry Hikaricp Dashboard URL"
-}
