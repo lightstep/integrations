bcc9aa64b2b022086fcc277bb7f904e35b96188f
Dmitry Avdeev <dmitry.avdeev@lightstep.com>
2023-09-14 09:37:26 -0700

    change back to lightstep project

--- azure/containerinstance_containergroups/dashboards/overview/main.tf
+++ azure/containerinstance_containergroups/dashboards/overview/main.tf
@@ -8,18 +8,18 @@ terraform {
   required_version = ">= v1.0.11"
 }
 
-variable "cloud_observability_project" {
+variable "lightstep_project" {
   description = "Cloud Observability Project Name"
   type        = string
 }
 
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.cloud_observability_project}/dashboard/${lightstep_dashboard.otel_collector_dashboard.id}"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.otel_collector_dashboard.id}"
   description = "OpenTelemetry Collector Container Instance Container Groups Dashboard URL"
 }
 
 resource "lightstep_dashboard" "otel_collector_dashboard" {
-  project_name   = var.cloud_observability_project
+  project_name   = var.lightstep_project
   dashboard_name = "Container Instance Container Groups Metrics"
   dashboard_description = "[Beta] Monitor Container Instance Container Groups with this metrics overview dashboard."
 
