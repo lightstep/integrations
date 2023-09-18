ec189f397c07dadf9fa28674f3e3f76fe503cbc0
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-09-18 16:12:02 -0500

    change names

--- collector/httpcheck/dashboards/overview/main.tf
+++ collector/httpcheck/dashboards/overview/main.tf
@@ -9,9 +9,9 @@ terraform {
 }
 
 
-resource "lightstep_dashboard" "otel_collector_httpcheck_dashboard" {
-  project_name          = var.cloud_observability_project
-  dashboard_name        = "OpenTelemetry HttpCheck Dashboard"
+resource "lightstep_dashboard" "collector_httpcheck_overview" {
+  project_name          = var.lightstep_project
+  dashboard_name        = "OpenTelemetry HTTPCheck Dashboard"
   dashboard_description = "Monitor the HTTPCheck of your OpenTelemetry Collector instances"
 
   chart {
