423aaa1a8b9eb1fe8db03ebbceb3e2d408dcce89
Dmitry Avdeev <dmitry.avdeev@lightstep.com>
2023-09-19 14:30:59 -0700

    dashboards names fixes

--- azure/compute_virtualmachinescalesets_virtualmachines/dashboards/overview/main.tf
+++ azure/compute_virtualmachinescalesets_virtualmachines/dashboards/overview/main.tf
@@ -14,7 +14,7 @@ variable "lightstep_project" {
 }
 
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.otel_collector_dashboard.id}"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.azure_compute_virtualmachinescalesets_virtualmachines_overview.id}"
   description = "OpenTelemetry Collector Compute Virtual Machine Scale Sets Virtual Machines Dashboard URL"
 }
 
--- azure/documentdb_mongocluster/dashboards/overview/main.tf
+++ azure/documentdb_mongocluster/dashboards/overview/main.tf
@@ -14,7 +14,7 @@ variable "lightstep_project" {
 }
 
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.otel_collector_dashboard.id}"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.azure_documentdb_mongocluster_overview.id}"
   description = "OpenTelemetry Collector DocumentDB Mongo Cluster Dashboard URL"
 }
 
