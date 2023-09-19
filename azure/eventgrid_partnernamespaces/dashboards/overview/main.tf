720e8778a0532d11722369dad39efb3e21e278e3
Dmitry Avdeev <dmitry.avdeev@lightstep.com>
2023-09-19 11:19:50 -0700

    azure dashboards group #4

---
+++ azure/eventgrid_partnernamespaces/dashboards/overview/main.tf
@@ -0,0 +1,121 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = "~> 1.76.0"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
+variable "lightstep_project" {
+  description = "Cloud Observability Project Name"
+  type        = string
+}
+
+output "dashboard_url" {
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.azure_eventgrid_partnernamespaces_overview.id}"
+  description = "OpenTelemetry Collector Event Grid Partner Namespaces Dashboard URL"
+}
+
+resource "lightstep_dashboard" "azure_eventgrid_partnernamespaces_overview" {
+  project_name          = var.lightstep_project
+  dashboard_name        = "Event Grid Partner Namespaces Metrics"
+  dashboard_description = "[Beta] Monitor Event Grid Partner Namespaces with this metrics overview dashboard."
+
+  chart {
+    name = "Publish Failed Events"
+    rank = "0"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_publishfailcount_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_errortype == $metadata_errortype) && (metadata_error == $metadata_error)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Published Events"
+    rank = "1"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_publishsuccesscount_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Unmatched Events"
+    rank = "2"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_unmatchedeventcount_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Publish Success Latency"
+    rank = "3"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_publishsuccesslatencyinms_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  template_variable {
+    name                     = "azuremonitor_resource_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.resource_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_subscription_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.subscription_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_tenant_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.tenant_id"
+  }
+
+  template_variable {
+    name                     = "location"
+    default_values           = []
+    suggestion_attribute_key = "location"
+  }
+
+  template_variable {
+    name                     = "metadata_error"
+    default_values           = []
+    suggestion_attribute_key = "metadata_error"
+  }
+
+  template_variable {
+    name                     = "metadata_errortype"
+    default_values           = []
+    suggestion_attribute_key = "metadata_errortype"
+  }
+}
