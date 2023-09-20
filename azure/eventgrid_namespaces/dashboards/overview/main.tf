ee52fea5b3e6eba855ce9e91069192100064f320
Dmitry Av <altuner@users.noreply.github.com>
2023-09-20 10:52:49 -0700

    Azure dashboards group 4 (#47)
    Co-authored-by: Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>

---
+++ azure/eventgrid_namespaces/dashboards/overview/main.tf
@@ -0,0 +1,307 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = "~> 1.76.0"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
+variable "lightstep_project" {
+  description = "Cloud Observability Project Name"
+  type        = string
+}
+
+output "dashboard_url" {
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.azure_eventgrid_namespaces_overview.id}"
+  description = "OpenTelemetry Collector Event Grid Namespaces Dashboard URL"
+}
+
+resource "lightstep_dashboard" "azure_eventgrid_namespaces_overview" {
+  project_name          = var.lightstep_project
+  dashboard_name        = "Event Grid Namespaces Metrics"
+  dashboard_description = "[Beta] Monitor Event Grid Namespaces with this metrics overview dashboard."
+
+  chart {
+    name = "Failed Acknowledged Events"
+    rank = "0"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_failedacknowledgedevents_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_eventsubscriptionname == $metadata_eventsubscriptionname) && (metadata_error == $metadata_error) && (metadata_errortype == $metadata_errortype)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Successful Acknowledged Events"
+    rank = "1"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_successfulacknowledgedevents_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_eventsubscriptionname == $metadata_eventsubscriptionname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Failed Publish Events"
+    rank = "2"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_failedpublishedevents_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_error == $metadata_error) && (metadata_errortype == $metadata_errortype)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Successful Publish Events"
+    rank = "3"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_successfulpublishedevents_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Failed Received Events"
+    rank = "4"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_failedreceivedevents_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_eventsubscriptionname == $metadata_eventsubscriptionname) && (metadata_error == $metadata_error) && (metadata_errortype == $metadata_errortype)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Successful Received Events"
+    rank = "5"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_successfulreceivedevents_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_eventsubscriptionname == $metadata_eventsubscriptionname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Failed Released Events"
+    rank = "6"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_failedreleasedevents_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_eventsubscriptionname == $metadata_eventsubscriptionname) && (metadata_error == $metadata_error) && (metadata_errortype == $metadata_errortype)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Successful Released Events"
+    rank = "7"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_successfulreleasedevents_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_eventsubscriptionname == $metadata_eventsubscriptionname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "MQTT: Connections"
+    rank = "8"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_mqtt.connections_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "MQTT: Failed Published Messages"
+    rank = "9"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_mqtt.failedpublishedmessages_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_error == $metadata_error)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "MQTT: Successful Published Messages"
+    rank = "10"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_mqtt.successfulpublishedmessages_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "MQTT: Failed Subscription Operations"
+    rank = "11"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_mqtt.failedsubscriptionoperations_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_error == $metadata_error)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "MQTT: Successful Subscription Operations"
+    rank = "12"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_mqtt.successfulsubscriptionoperations_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "MQTT: Failed Routed Messages"
+    rank = "13"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_mqtt.failedroutedmessages_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_error == $metadata_error)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "MQTT: Successful Routed Messages"
+    rank = "14"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_mqtt.successfulroutedmessages_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Publish Operations Latency"
+    rank = "15"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_publishlatencyinmilliseconds_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  template_variable {
+    name                     = "azuremonitor_resource_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.resource_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_subscription_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.subscription_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_tenant_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.tenant_id"
+  }
+
+  template_variable {
+    name                     = "location"
+    default_values           = []
+    suggestion_attribute_key = "location"
+  }
+
+  template_variable {
+    name                     = "metadata_error"
+    default_values           = []
+    suggestion_attribute_key = "metadata_error"
+  }
+
+  template_variable {
+    name                     = "metadata_errortype"
+    default_values           = []
+    suggestion_attribute_key = "metadata_errortype"
+  }
+
+  template_variable {
+    name                     = "metadata_eventsubscriptionname"
+    default_values           = []
+    suggestion_attribute_key = "metadata_eventsubscriptionname"
+  }
+}
