7152cfce8c1b36fa5694fa4736cb8911e09d3d34
Dmitry Avdeev <dmitry.avdeev@lightstep.com>
2023-09-25 14:45:52 -0700

    azure dashboards group #5

---
+++ azure/eventhub_namespaces/dashboards/overview/main.tf
@@ -0,0 +1,292 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = "~> 1.76.0"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
+variable "lightstep_project" {
+  description = "Cloud Observability Project Name"
+  type        = string
+}
+
+output "dashboard_url" {
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.azure_eventhub_namespaces_overview.id}"
+  description = "OpenTelemetry Collector Event Hub Namespaces Dashboard URL"
+}
+
+resource "lightstep_dashboard" "azure_eventhub_namespaces_overview" {
+  project_name          = var.lightstep_project
+  dashboard_name        = "Event Hub Namespaces Metrics"
+  dashboard_description = "[Beta] Monitor Event Hub Namespaces with this metrics overview dashboard."
+
+  chart {
+    name = "Active Connections"
+    rank = "0"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_activeconnections_maximum | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "CPU"
+    rank = "1"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_namespacecpuusage_maximum | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_replica == $metadata_replica)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Memory Usage"
+    rank = "2"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_namespacememoryusage_maximum | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_replica == $metadata_replica)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Successful Requests"
+    rank = "3"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_successfulrequests_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname) && (metadata_operationresult == $metadata_operationresult)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "User Errors"
+    rank = "4"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_usererrors_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname) && (metadata_operationresult == $metadata_operationresult)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Server Errors"
+    rank = "5"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_servererrors_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname) && (metadata_operationresult == $metadata_operationresult)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Quota Exceeded Errors"
+    rank = "6"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_quotaexceedederrors_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname) && (metadata_operationresult == $metadata_operationresult)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Connections Closed"
+    rank = "7"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_connectionsclosed_maximum | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Connections Opened"
+    rank = "8"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_connectionsopened_maximum | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Incoming Messages"
+    rank = "9"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_incomingmessages_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Outgoing Messages"
+    rank = "10"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_outgoingmessages_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Incoming Bytes"
+    rank = "11"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_incomingbytes_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Outgoing Bytes"
+    rank = "12"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_outgoingbytes_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Captured Messages"
+    rank = "13"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_capturedmessages_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Captured Bytes"
+    rank = "14"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_capturedbytes_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_entityname == $metadata_entityname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  template_variable {
+    name                     = "azuremonitor_resource_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.resource_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_subscription_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.subscription_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_tenant_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.tenant_id"
+  }
+
+  template_variable {
+    name                     = "location"
+    default_values           = []
+    suggestion_attribute_key = "location"
+  }
+
+  template_variable {
+    name                     = "metadata_entityname"
+    default_values           = []
+    suggestion_attribute_key = "metadata_entityname"
+  }
+
+  template_variable {
+    name                     = "metadata_operationresult"
+    default_values           = []
+    suggestion_attribute_key = "metadata_operationresult"
+  }
+
+  template_variable {
+    name                     = "metadata_replica"
+    default_values           = []
+    suggestion_attribute_key = "metadata_replica"
+  }
+}
---
+++ azure/keyvault_vaults/dashboards/overview/main.tf
@@ -0,0 +1,154 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = "~> 1.76.0"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
+variable "lightstep_project" {
+  description = "Cloud Observability Project Name"
+  type        = string
+}
+
+output "dashboard_url" {
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.azure_keyvault_vaults_overview.id}"
+  description = "OpenTelemetry Collector Key Vault Vaults Dashboard URL"
+}
+
+resource "lightstep_dashboard" "azure_keyvault_vaults_overview" {
+  project_name          = var.lightstep_project
+  dashboard_name        = "Key Vault Vaults Metrics"
+  dashboard_description = "[Beta] Monitor Key Vault Vaults with this metrics overview dashboard."
+
+  chart {
+    name = "Overall Vault Availability"
+    rank = "0"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_availability_average | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_activitytype == $metadata_activitytype) && (metadata_activityname == $metadata_activityname) && (metadata_statuscode == $metadata_statuscode) && (metadata_statuscodeclass == $metadata_statuscodeclass)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Overall Vault Saturation"
+    rank = "1"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_saturationshoebox_average | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_activitytype == $metadata_activitytype) && (metadata_activityname == $metadata_activityname) && (metadata_transactiontype == $metadata_transactiontype)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Total Service Api Hits"
+    rank = "2"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_serviceapihit_count | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_activitytype == $metadata_activitytype) && (metadata_activityname == $metadata_activityname)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Total Service Api Results"
+    rank = "3"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_serviceapiresult_count | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_activitytype == $metadata_activitytype) && (metadata_activityname == $metadata_activityname) && (metadata_statuscode == $metadata_statuscode) && (metadata_statuscodeclass == $metadata_statuscodeclass)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Overall Service Api Latency"
+    rank = "4"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_serviceapilatency_average | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_activitytype == $metadata_activitytype) && (metadata_activityname == $metadata_activityname) && (metadata_statuscode == $metadata_statuscode) && (metadata_statuscodeclass == $metadata_statuscodeclass)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  template_variable {
+    name                     = "azuremonitor_resource_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.resource_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_subscription_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.subscription_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_tenant_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.tenant_id"
+  }
+
+  template_variable {
+    name                     = "location"
+    default_values           = []
+    suggestion_attribute_key = "location"
+  }
+
+  template_variable {
+    name                     = "metadata_activityname"
+    default_values           = []
+    suggestion_attribute_key = "metadata_activityname"
+  }
+
+  template_variable {
+    name                     = "metadata_activitytype"
+    default_values           = []
+    suggestion_attribute_key = "metadata_activitytype"
+  }
+
+  template_variable {
+    name                     = "metadata_statuscode"
+    default_values           = []
+    suggestion_attribute_key = "metadata_statuscode"
+  }
+
+  template_variable {
+    name                     = "metadata_statuscodeclass"
+    default_values           = []
+    suggestion_attribute_key = "metadata_statuscodeclass"
+  }
+
+  template_variable {
+    name                     = "metadata_transactiontype"
+    default_values           = []
+    suggestion_attribute_key = "metadata_transactiontype"
+  }
+}
