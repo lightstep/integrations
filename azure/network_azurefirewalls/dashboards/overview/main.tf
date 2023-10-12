b65a7e07362bb8339bc30c8305a798a195be689f
Dmitry Avdeyev <dmitry.avdeev@lightstep.com>
2023-10-11 17:22:55 -0700

    Azure dashboards group 6

---
+++ azure/network_azurefirewalls/dashboards/overview/main.tf
@@ -0,0 +1,172 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = "~> 1.76.0"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
+variable "lightstep_project" {
+  description = "Cloud Observability Project Name"
+  type        = string
+}
+
+output "dashboard_url" {
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.azure_network_azurefirewalls_overview.id}"
+  description = "OpenTelemetry Collector Network Azure Firewalls Dashboard URL"
+}
+
+resource "lightstep_dashboard" "azure_network_azurefirewalls_overview" {
+  project_name          = var.lightstep_project
+  dashboard_name        = "Network Azure Firewalls Metrics"
+  dashboard_description = "[Beta] Monitor Network Azure Firewalls with this metrics overview dashboard."
+
+  chart {
+    name = "Application Rule Hits"
+    rank = "0"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_applicationrulehit_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_status == $metadata_status) && (metadata_reason == $metadata_reason) && (metadata_protocol == $metadata_protocol)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Data Processed"
+    rank = "1"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_dataprocessed_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Firewall Health State"
+    rank = "2"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_firewallhealth_average | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_status == $metadata_status) && (metadata_reason == $metadata_reason)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Latency Probe"
+    rank = "3"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_firewalllatencypng_average | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Network Rule Hits"
+    rank = "4"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_networkrulehit_total | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_status == $metadata_status) && (metadata_reason == $metadata_reason) && (metadata_protocol == $metadata_protocol)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "SNAT Port Utilization"
+    rank = "5"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_snatportutilization_average | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location) && (metadata_protocol == $metadata_protocol)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  chart {
+    name = "Throughput"
+    rank = "6"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric azure_throughput_average | filter ((azuremonitor.subscription_id == $azuremonitor_subscription_id) && (azuremonitor.tenant_id == $azuremonitor_tenant_id) && (azuremonitor.resource_id == $azuremonitor_resource_id) && (location == $location)) | delta | group_by [], sum
+EOT
+    }
+  }
+
+  template_variable {
+    name                     = "azuremonitor_resource_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.resource_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_subscription_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.subscription_id"
+  }
+
+  template_variable {
+    name                     = "azuremonitor_tenant_id"
+    default_values           = []
+    suggestion_attribute_key = "azuremonitor.tenant_id"
+  }
+
+  template_variable {
+    name                     = "location"
+    default_values           = []
+    suggestion_attribute_key = "location"
+  }
+
+  template_variable {
+    name                     = "metadata_protocol"
+    default_values           = []
+    suggestion_attribute_key = "metadata_protocol"
+  }
+
+  template_variable {
+    name                     = "metadata_reason"
+    default_values           = []
+    suggestion_attribute_key = "metadata_reason"
+  }
+
+  template_variable {
+    name                     = "metadata_status"
+    default_values           = []
+    suggestion_attribute_key = "metadata_status"
+  }
+}
