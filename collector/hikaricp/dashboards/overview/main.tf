ee6d3351cafa037139acf577407dc432863d5500
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-10-25 06:53:44 -0500

    fix errant cloud_observability_ prefixes (#65)

--- collector/hikaricp/dashboards/overview/main.tf
+++ collector/hikaricp/dashboards/overview/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 resource "lightstep_dashboard" "otel_collector_hikaricp_dashboard" {
-  project_name          = var.cloud_observability_project
+  project_name          = var.lightstep_project
   dashboard_name        = "HikariCP and System Metrics Dashboard"
   dashboard_description = "Monitor HikariCP and System Metrics."
 
--- collector/hive/dashboards/overview/main.tf
+++ collector/hive/dashboards/overview/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 resource "lightstep_dashboard" "otel_collector_hive_dashboard" {
-  project_name          = var.cloud_observability_project
+  project_name          = var.lightstep_project
   dashboard_name        = "OpenTelemetry Hive JMX Metrics Dashboard"
   dashboard_description = "Monitor Hive JMX metrics with this overview dashboard."
 
