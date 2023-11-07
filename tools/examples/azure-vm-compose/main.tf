505fbc1a079d995b67a99adab3c47cf7550f0004
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-11-07 15:11:12 -0600

    add bastion and renames

--- tools/examples/azure-vm-compose/main.tf
+++ tools/examples/azure-vm-compose/main.tf
@@ -2,49 +2,79 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "compose_example" {
-  name     = "kongComposeResourceGroup"
+resource "azurerm_resource_group" "kong_compose_example" {
+  name     = "kong_compose_example-resources"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "compose_example" {
-  name                = "kongComposeVNet"
+resource "azurerm_virtual_network" "kong_compose_example" {
+  name                = "kong_compose_example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.compose_example.location
-  resource_group_name = azurerm_resource_group.compose_example.name
+  location            = azurerm_resource_group.kong_compose_example.location
+  resource_group_name = azurerm_resource_group.kong_compose_example.name
 }
 
-resource "azurerm_subnet" "compose_example" {
-  name                 = "kongComposeSubnet"
-  resource_group_name  = azurerm_resource_group.compose_example.name
-  virtual_network_name = azurerm_virtual_network.compose_example.name
+resource "azurerm_subnet" "kong_compose_example" {
+  name                 = "kong_compose_example-subnet"
+  resource_group_name  = azurerm_resource_group.kong_compose_example.name
+  virtual_network_name = azurerm_virtual_network.kong_compose_example.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "compose_example" {
-  name                = "kongComposeNIC"
-  location            = azurerm_resource_group.compose_example.location
-  resource_group_name = azurerm_resource_group.compose_example.name
+resource "azurerm_network_security_group" "kong_compose_example" {
+  name                = "kong_compose_example-nsg"
+  location            = azurerm_resource_group.kong_compose_example.location
+  resource_group_name = azurerm_resource_group.kong_compose_example.name
 
-  ip_configuration {
-    name                          = "kongComposeNICConfiguration"
-    subnet_id                     = azurerm_subnet.compose_example.id
-    private_ip_address_allocation = "Dynamic"
+  security_rule {
+    name                       = "AllowBastionInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "AzureBastionSubnet"
+    destination_address_prefix = "*"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "compose_example" {
-  name                = "kongComposeVM"
-  resource_group_name = azurerm_resource_group.compose_example.name
-  location            = azurerm_resource_group.compose_example.location
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.kong_compose_example.name
+  virtual_network_name = azurerm_virtual_network.kong_compose_example.name
+  address_prefixes     = ["10.0.2.0/27"]
+}
+
+resource "azurerm_public_ip" "bastion_public_ip" {
+  name                = "kong_compose_example-bastion-ip"
+  location            = azurerm_resource_group.kong_compose_example.location
+  resource_group_name = azurerm_resource_group.kong_compose_example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "kong_compose_example" {
+  name                = "kong_compose_example-bastion-host"
+  location            = azurerm_resource_group.kong_compose_example.location
+  resource_group_name = azurerm_resource_group.kong_compose_example.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "kong_compose_example" {
+  name                = "kong_compose_example-vm"
+  resource_group_name = azurerm_resource_group.kong_compose_example.name
+  location            = azurerm_resource_group.kong_compose_example.location
   size                = "Standard_DS1_v2"
   admin_username      = "azureuser"
-  admin_password      = "Strong_2_Secure"
-  disable_password_authentication = false
   network_interface_ids = [
-    azurerm_network_interface.compose_example.id,
+    azurerm_network_interface.kong_compose_example.id,
   ]
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -57,37 +87,18 @@ resource "azurerm_linux_virtual_machine" "compose_example" {
     version   = "latest"
   }
 
-  custom_data = filebase64("${path.module}/setup.sh")
+  custom_data = base64encode(file("${path.module}/setup.sh"))
 }
 
-resource "azurerm_public_ip" "compose_example" {
-  name                = "kongComposePublicIP"
-  location            = azurerm_resource_group.compose_example.location
-  resource_group_name = azurerm_resource_group.compose_example.name
-  allocation_method   = "Dynamic"
-}
+resource "azurerm_network_interface" "kong_compose_example" {
+  name                = "kong_compose_example-nic"
+  location            = azurerm_resource_group.kong_compose_example.location
+  resource_group_name = azurerm_resource_group.kong_compose_example.name
 
-resource "azurerm_network_security_group" "compose_example" {
-  name                = "kongComposeNSG"
-  location            = azurerm_resource_group.compose_example.location
-  resource_group_name = azurerm_resource_group.compose_example.name
-
-  security_rule {
-    name                       = "DenyAllInbound"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.kong_compose_example.id
+    private_ip_address_allocation = "Dynamic"
   }
-
-}
-
-resource "azurerm_network_interface_security_group_association" "compose_example" {
-  network_interface_id      = azurerm_network_interface.compose_example.id
-  network_security_group_id = azurerm_network_security_group.compose_example.id
 }
 
