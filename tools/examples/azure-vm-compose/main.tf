f83af3942e003445dc62aa850a147c2c472b0a97
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-11-08 12:14:41 -0600

    Work on systemd service

--- tools/examples/azure-vm-compose/main.tf
+++ tools/examples/azure-vm-compose/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_network_security_group" "kong_compose_example" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "AzureBastionSubnet"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
@@ -47,7 +47,7 @@ resource "azurerm_subnet" "bastion_subnet" {
 }
 
 resource "azurerm_public_ip" "bastion_public_ip" {
-  name                = "kong_compose_example-bastion-ip"
+  name                = "kong-compose-example-bastion-ip"
   location            = azurerm_resource_group.kong_compose_example.location
   resource_group_name = azurerm_resource_group.kong_compose_example.name
   allocation_method   = "Static"
@@ -66,15 +66,41 @@ resource "azurerm_bastion_host" "kong_compose_example" {
   }
 }
 
+variable "LS_ACCESS_TOKEN" {
+  description = "The ServiceNow Cloud Observability access token"
+  type        = string
+  sensitive   = true
+}
+
 resource "azurerm_linux_virtual_machine" "kong_compose_example" {
-  name                = "kong_compose_example-vm"
+  name                = "kong-compose-example-vm"
   resource_group_name = azurerm_resource_group.kong_compose_example.name
   location            = azurerm_resource_group.kong_compose_example.location
   size                = "Standard_DS1_v2"
   admin_username      = "azureuser"
+
+  /*
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "azureuser"
+  }
+  */
+
+  // os_profile_linux_config {
+  //  disable_password_authentication = true
+  // }
+  
+  // admin_password      = "Azure_2_Password"
+  admin_ssh_key {
+    username  = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  // disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.kong_compose_example.id,
   ]
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -87,7 +113,24 @@ resource "azurerm_linux_virtual_machine" "kong_compose_example" {
     version   = "latest"
   }
 
-  custom_data = base64encode(file("${path.module}/setup.sh"))
+  /*
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'export LS_ACCESS_TOKEN=${var.LS_ACCESS_TOKEN}' >> /etc/profile.d/lightstep.sh"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "azureuser"
+      private_key = file("~/.ssh/id_rsa")
+      host     = self.public_ip_address
+    }
+  }
+  */
+
+  custom_data = base64encode(templatefile("${path.module}/setup.sh", {
+    LS_ACCESS_TOKEN = var.LS_ACCESS_TOKEN
+  }))
 }
 
 resource "azurerm_network_interface" "kong_compose_example" {
--- tools/examples/azure-vm-compose/setup.sh
+++ tools/examples/azure-vm-compose/setup.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-apt-get update
-apt-get install -y docker.io docker-compose git
-usermod -aG docker azureuser
+#
+# Setup Debian flavor instance to run docker-compose example for kong
 
-# mkdir -p /home/azureuser/integrations
-git clone https://github.com/lightstep/integrations
-chown -R azureuser:azureuser /home/azureuser/integrations
+function setup_tools() {
+  apt-get update
+  apt-get install -y docker.io docker-compose git
+  usermod -aG docker azureuser
+}
 
+function setup_code() {
+  mkdir -p /home/azureuser/integrations
+  cd /home/azureuser/integrations || exit
+  git clone https://github.com/lightstep/integrations .
+  chown -R azureuser:azureuser /home/azureuser/integrations
+}
+
+function set_environment() {
+  # Set environment variables for docker-compose to persist across reboots
+  # single quotes aren't expanded, so the string is written literally
+  # shellcheck disable=SC2016
+  echo 'export LS_ACCESS_TOKEN=${LS_ACCESS_TOKEN}' >> /etc/profile.d/lightstep.sh
+}
+
+function make_compose_app_service() {
 cat <<EOF > /etc/systemd/system/docker-compose-app.service
 [Unit]
 Description=Docker Compoose Application Service
@@ -16,19 +32,47 @@ After=docker.service network.target
 [Service]
 Type=oneshot
 RemainAfterExit=yes
-WorkingDirectory=home/azureuser/integrations/collector/kong/examples/compose
-ExecStart=/user/bin/docker-compose up -d
-ExecStop=/user/bin/docker-compose down
-TiomeoutStartSec=0
+WorkingDirectory=/home/azureuser/integrations/collector/kong/examples/compose
+ExecStart=/usr/bin/docker-compose up -d
+ExecStop=/usr/bin/docker-compose down
+TimeoutStartSec=0
 
 [Install]
 WantedBy=multi-user.target
 EOF
+}
 
-# Reload systemd daemon to run our new service
-systemctl daemon-reload
+function start_service() {
+  # Reload systemd daemon to run our new service
+  systemctl daemon-reload
 
-# Enable and start
-systemctl enable docker-compose-app.service
-systemctl start docker-compose-app.service
+  # Enable and start
+  systemctl enable docker-compose-app.service
+  systemctl start docker-compose-app.service
+}
 
+function checkout_if_exists() {
+  local branch=$1
+
+  # check if branch exists locally
+  if git rev-parse --verify $branch >/dev/null 2>&1; then
+    echo "Switching to branch '$branch'."
+    git checkout $branch
+  elif git fetch origin $branch && git rev-parse --verify origin/$branch >/dev/null 2>&1; then
+    echo "Branch '$branch' found in remote. Checking out and tracking."
+    git checkout -t origin/$branch
+  else
+    echo "Branch '$branch' does not exist."
+  fi
+}
+
+function main() {
+  setup_tools
+  setup_code
+  checkout_if_exists njs/add-kong-compose-eg
+  set_environment
+  make_compose_app_service
+  start_app_service
+}
+
+main "$@"
