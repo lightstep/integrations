f9b49058bcd799cf361255d4e17b5b48a15cc7ef
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-11-27 13:26:56 -0600

    Add all signals in 1 Kong example (#71)

--- collector/kong/examples/compose/README.md
+++ collector/kong/examples/compose/README.md
@@ -1,6 +1,28 @@
-# Kong Example
+# Kong API Gateway Example
 
-```
-export LS_ACCESS_TOKEN="my-access-token"
-docker-compose up
+## Overview
+
+This example demonstrates monitoring Kong's API Gateway with the OpenTelemetry Collector and ServiceNow Cloud Observability.
+
+Kong is configured to send metrics and traces to the OpenTelemetry Collector. The OpenTelemetry Collector is configured to send metrics to and traces to ServiceNow Cloud Observability.
+
+## Requirements
+
+This example expects a project access token in the environment variable `LS_ACCESS_TOKEN`. A line like the following will work for setting environment in a shell profile.
+
+`export LS_ACCESS_TOKEN=<<"your-access-token-here">>`
+
+## Make targets
+
+The Makefile has the following targets:
+
+```shell
+  run      Run the kong-gateway, client and otel-collector containers (default goal)
+  check    Check kong.conf and kong.yml
+  shell    /bin/bash in the kong container
+  curl     curls the config and routes
+  stop     Stop services
+  start    Start services
+  reload   Tear it down and bring it back up
+  help     Show this help.
 ```
--- collector/kong/examples/compose/app/client.py
+++ collector/kong/examples/compose/app/client.py
@@ -3,63 +3,31 @@
 # perform requests to a local kong service
 #
 #   export DESTINATION_HOST=my-host-address
-#   export COLLECTOR_HOST=my-collector-host
 #   python client.py
 
 import os
 import time
 import requests
-import sys
 
-def config_kong(host, collector):
-    while True:
-        try:
-            requests.get(f"http://{host}:8001/")
-            break
-        except Exception as e:
-            print("Kong API is not running yet, sleeping")
-            time.sleep(1)
-            continue
+gateway_port = 8000
 
-    try:
-        # Configure the OpenTelemetry plugin
-        res = requests.post(f"http://{host}:8001/plugins", json={
-            'name': 'opentelemetry',
-            'config': {
-                'endpoint': f"http://{collector}:4318/v1/traces",
-                'resource_attributes': { 'service.name': 'kong-dev' },
-            },
-        })
-        print(res.text)
-        # Create the service
-        res = requests.post(f"http://{host}:8001/services", json={
-            'name': 'servicenow_service',
-            'url': 'http://www.servicenow.com',
-        })
-        print(res.text)
-        # Create the route
-        res = requests.post(f"http://{host}:8001/services/servicenow_service/routes", json={
-            'paths': ['/servicenow'],
-            'name': 'servicenow_route',
-        })
-        print(res.text)
-    except Exception as e:
-        print(f"Initial configuration failed: {e}")
-        sys.exit(1)
-
-def send_requests(url):
+def send_requests(url: str):
     try:
         res = requests.get(url)
-        print(f"Request to {url}, got {len(res.content)} bytes")
+        print(f"Request to {url}, status: {res.status_code} got {len(res.content)} bytes")
+        if res.status_code >= 400:
+            try:
+                json_response = res.json()
+                print("Message: ", json_response.get('message', 'Kong provided no message'))
+            except ValueError:
+                print('No json body in response')
     except Exception as e:
         print(f"Request to {url} failed {e}")
 
 if __name__ == "__main__":
     host = os.getenv("DESTINATION_HOST", "localhost")
-    collector = os.getenv("COLLECTOR_HOST", "localhost")
-    config_kong(host, collector)
 
-    target = f"http://{host}:8000/servicenow/company.html"
+    target = f"http://{host}:{gateway_port}/httpbin/bytes/111"
     while True:
         send_requests(target)
-        time.sleep(.5)
+        time.sleep(1)
